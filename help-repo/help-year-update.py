52938720d43ae0f920fe5c053d805aae9949464a
KarimLak <karim.lakhal@polymtl.ca>
2023-05-22 16:14:10 -0700

    Revision for years attribute in lurelu

--- help-repo/help-year-update.py
+++ help-repo/help-year-update.py
@@ -1,12 +1,10 @@
 from rdflib import Graph, Namespace, Literal, XSD
-from datetime import datetime
 
 # Define the namespace
 ns1 = Namespace("http://schema.org/")
 
 # List of TTL files to process
 filepaths = [
-    "./output-lurelu-tables.ttl",
     "./output-lurelu.ttl"
 ]
 
@@ -21,13 +19,16 @@ for filepath in filepaths:
     for book in books:
         # Extract the current datePublished
         datePublished = g.value(subject=book, predicate=ns1["datePublished"])
+        # Extract the current award
+        award = str(g.value(subject=book, predicate=ns1["award"]))
 
-        if datePublished.datatype == XSD.gYear:
-            # Reduce the year by 1
-            reduced_year = int(datePublished) - 1
-
-            # Update the book's datePublished in the graph
-            g.set((book, ns1["datePublished"], Literal(reduced_year, datatype=XSD.gYear)))
+        if datePublished is not None and datePublished.datatype == XSD.gYear and award != 'Prix Cécile-Gagnon':
+            # Increase the year by 1
+            increased_year = int(datePublished) + 1
+            # Remove the old datePublished
+            g.remove((book, ns1["datePublished"], None))
+            # Update the book's datePublished in the graph to dateReceived
+            g.add((book, ns1["dateReceived"], Literal(increased_year, datatype=XSD.gYear)))
 
     # Serialize the updated graph
     output_data = g.serialize(format="turtle")
