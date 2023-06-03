cb7ae345d51126fe4fe68bbcdf9f7d0a27ceb2fa
KarimLak <karim.lakhal@polymtl.ca>
2023-06-03 01:58:06 -0400

    Award conversion

--- help_repo/test3.py
+++ help_repo/test3.py
@@ -1,50 +1,31 @@
-from rdflib import Graph, Namespace, URIRef
-import re
-import urllib.parse
+from rdflib import Graph
 
-# Namespaces
-ns1 = Namespace("http://schema.org/")
-mcc = Namespace("http://example.org/mcc/")
+g = Graph()
+g.parse("./new_awards.ttl", format="turtle", encoding="utf-8")
+g.parse("./1_awards_conversion_final_demo.ttl", format="turtle", encoding="utf-8")
 
-# Load the awards graph
-awards_g = Graph()
-awards_g.parse("./1_awards_conversion_final_demo.ttl", format="turtle", encoding="utf-8")
+qres = g.update("""
+    PREFIX ns1: <http://schema.org/>
+    PREFIX mcc: <http://example.com/mcc#> 
+    PREFIX pbs: <http://example.com/pbs#>
+    
+    INSERT {
+        ?award mcc:R37 ?book ;
+               mcc:MCC-R35-4 ?year .
+    }
+    WHERE {
+        ?book a ns1:Book ;
+            ns1:award ?award ;
+            ns1:dateReceived ?year .
+        ?award a mcc:MCC-E12 .
+        FILTER NOT EXISTS {?award mcc:R37 ?bookExisting .}
+        FILTER NOT EXISTS {?award mcc:MCC-R35-4 ?yearExisting .}
+    }
+""")
 
-# Function to find parent award URI
-def find_parent_award(award_uri):
-    parent_award = None
-    # Decode URL before finding parent
-    decoded_award_uri = urllib.parse.unquote(award_uri)
-    for s, p, o in awards_g.triples((None, ns1.subOrganization, URIRef(decoded_award_uri))):
-        parent_award = s
-        break
-    return parent_award
-
-def process_line(line):
-    match = re.match(r'(ns1:\S+) a mcc:MCC-E12 ;', line, re.DOTALL)
-    if match:
-        uri_str = match.group(1)
-        # Strip "ns1:" prefix
-        uri_str = uri_str[4:]
-        # Handle special characters
-        uri_str = urllib.parse.quote(uri_str, safe='')
-        award_uri = URIRef(f'http://schema.org/{uri_str}')
-        print(f'Award URI: {award_uri}')
-
-        # Find parent award
-        parent_award_uri = find_parent_award(str(award_uri))
-        print(f'Parent Award URI: {parent_award_uri}')
-        if parent_award_uri is not None:
-            parent_award_ref = parent_award_uri.split('/')[-1]
-            line = line.strip() + f'\n    ns1:award :{parent_award_ref} ;\n'
-    return line
-
-def process_file(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-
-    with open(output_file, 'w', encoding='utf-8') as f:
-        for line in lines:
-            f.write(process_line(line))
-
-process_file('./new_awards.ttl', './new_awards.ttl')
+# Write to output file
+with open('output.ttl', 'w', encoding='utf-8') as f:
+    for prefix, uri in g.namespaces():
+        f.write(f'@prefix {prefix}: <{uri}> .\n')
+    for s, p, o in g:
+        f.write(f'<{s}> <{p}> <{o}> .\n')
