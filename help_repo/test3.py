d3938849280e2cd3e90a0b312c77a6838cc445ad
KarimLak <karim.lakhal@polymtl.ca>
2023-06-04 13:13:27 -0400

    Adding dates for the awards

--- help_repo/test3.py
+++ help_repo/test3.py
@@ -1,48 +1,39 @@
 import re
-import collections
 
-award_to_books_map = collections.defaultdict(set)
-existing_awards = collections.defaultdict(set)
+# 1. Build a mapping of award IDs to dates from 1_awards_conversion_final_demo.ttl
+award_to_date_map = {}
 
-# Parse awards from the 1_final_output_merged_final_demo.ttl file
-with open('./1_final_output_merged_final_demo.ttl', 'r', encoding = "utf-8") as file:
+with open('./1_awards_conversion_final_demo.ttl', 'r', encoding="utf-8") as file:
     content = file.read()
-    blocks = content.split('\n\n')  # Split blocks more accurately 
+    blocks = content.split('\n\n')
 
     for block in blocks:
-        book_match = re.search(r'ns1:(Book\S+) a ns1:Book', block)
-        if book_match:
-            book_id = book_match.group(1)
-            award_matches = re.findall(r'ns1:award ns1:(\S+)', block)
-            for award_match in award_matches:
-                award_id = award_match
-                award_to_books_map[award_id].add(book_id)  
-
-# Identify awards that already have a book assigned in the new_awards.ttl file
-with open('./new_awards.ttl', 'r', encoding = "utf-8") as file:
-    content = file.read()
-    blocks = content.split('\n\n')  # Split blocks more accurately
-
-    for block in blocks:
-        award_match = re.search(r'ns1:(\S+) a mcc:MCC-E12', block)
+        award_match = re.search(r'ns1:(\S+) a ns1:Award', block)
         if award_match:
-            award_id = award_match.group(1)
-            book_match = re.search(r'mcc:R37 ns1:(Book\S+)', block)
-            if book_match:
-                existing_awards[award_id].add(book_match.group(1))
+            award_id = award_match.group(1).strip()
+            date_match = re.search(r'ns1:date "(\S+)"', block)
+            if date_match:
+                date = date_match.group(1).strip()
+                award_to_date_map[award_id] = date
 
-# Write to the new_awards.ttl file
+# 2. Update the new_awards.ttl file, adding the appropriate date triples
+with open('./new_awards.ttl', 'r', encoding="utf-8") as file:
+    content = file.read()
+
 new_content = ""
+blocks = content.split('\n\n')
+
 for block in blocks:
-    new_content += block
     award_match = re.search(r'ns1:(\S+) a mcc:MCC-E12', block)
     if award_match:
-        award_id = award_match.group(1)
-        if award_id in award_to_books_map:
-            for book_id in award_to_books_map[award_id]:
-                if book_id not in existing_awards[award_id]:
-                    new_content += f'\n    mcc:R37 ns1:{book_id} ;'
-    new_content += '\n\n'  # Recreate the block separator
+        award_id = award_match.group(1).strip()
+        if award_id in award_to_date_map:
+            date = award_to_date_map[award_id]
+            # Check if there are less than three attributes
+            attributes = block.split('\n')
+            if len(attributes) <= 3:
+                block = block.rstrip() + f'\n    mcc:MCC-R35-4 "{date}"^^xsd:gYear .'
+    new_content += block + "\n\n"
 
-with open('./new_awards.ttl', 'w', encoding = "utf-8") as file:
-    file.write(new_content.strip())  # Remove trailing newlines
+with open('./new_awards.ttl', 'w', encoding="utf-8") as file:
+    file.write(new_content.strip())
