60f0c7fdaec8595b94d0f791e295c9bf55c15f4e
KarimLak <karim.lakhal@polymtl.ca>
2023-06-07 19:56:38 -0400

    Correction of RDF errors in new_awards modelisation

--- help_repo/test_4.py
+++ help_repo/test_4.py
@@ -1,24 +1,14 @@
-def modify_ttl(file_path):
+def escape_quotes_in_file(file_path):
     with open(file_path, 'r') as f:
         content = f.readlines()
 
     new_content = []
-    for i, line in enumerate(content):
-        # If line is not empty
-        if line.strip():
-            # Check if we're not at the last line before trying to access i+1
-            if i+1 < len(content) and content[i+1].strip() == '':
-                new_line = line.rstrip()  # Remove trailing spaces
-                new_line = new_line[:-1] + ".\n"  # Replace last character with "."
-            else:
-                new_line = line.rstrip()  # Remove trailing spaces
-                new_line = new_line[:-1] + ";\n"  # Replace last character with ";"
-            new_content.append(new_line)
-        else:
-            new_content.append(line)
+    for line in content:
+        new_line = line.replace('"', '\\"')
+        new_content.append(new_line)
 
     with open(file_path, 'w') as f:
         for line in new_content:
             f.write(line)
 
-modify_ttl('./new_awards.ttl')
+escape_quotes_in_file('./new_awards.ttl')
