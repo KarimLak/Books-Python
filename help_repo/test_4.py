3300df30987e08be3c21d230b500a1f37d347a70
KarimLak <karim.lakhal@polymtl.ca>
2023-06-07 21:37:58 -0400

    Adding url attributes

--- help_repo/test_4.py
+++ help_repo/test_4.py
@@ -1,14 +1,23 @@
-def escape_quotes_in_file(file_path):
+def add_url_attribute(file_path, output_file_path, url_string):
     with open(file_path, 'r') as f:
         content = f.readlines()
 
     new_content = []
+    current_block = []
     for line in content:
-        new_line = line.replace('"', '\\"')
-        new_content.append(new_line)
+        current_block.append(line)
+        if line.strip().endswith('.'):
+            # Process the block
+            if not any('ns1:url' in l for l in current_block):
+                # Add the URL attribute if not present
+                current_block.insert(-1, f'    ns1:url "{url_string}"^^xsd:string ;\n')
+            new_content.extend(current_block)
+            current_block = []
 
-    with open(file_path, 'w') as f:
+    with open(output_file_path, 'w') as f:
         for line in new_content:
             f.write(line)
 
-escape_quotes_in_file('./new_awards.ttl')
+
+url_string = "https://livresgg.ca/gagnants-et-finalistes-precedents"
+add_url_attribute('./missing_output_1.ttl', './missing_output_1.ttl', url_string)
