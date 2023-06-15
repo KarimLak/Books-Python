b8d3e01c9cfc65760d7953359d0b102eec79b92e
KarimLak <karim.lakhal@polymtl.ca>
2023-06-15 16:56:34 -0400

    Refactor awards

--- test_5.py
+++ test_5.py
@@ -1,23 +1,27 @@
-import rdflib
+from rdflib import Graph, Namespace
+from rdflib.namespace import SKOS
 
-# Load the awards graph
-awards = rdflib.Graph()
-awards.parse("./awards.ttl", format="ttl")
+# Load your files
+awards_graph = Graph()
+awards_graph.parse('awards.ttl', format='turtle')
 
-# Define the namespaces
-mcc = rdflib.Namespace("http://example.com/mcc#")
-schema = rdflib.Namespace("http://schema.org/")
+hierarchy_graph = Graph()
+hierarchy_graph.parse('hierarchy_representation_awards.ttl', format='turtle')
 
-# Find awards without mcc:R37 attribute
-awards_without_r37 = awards.query('''
-    SELECT ?award {
-        ?award a mcc:MCC-E12 ;
-               ?p ?o .
-        FILTER NOT EXISTS { ?award mcc:R37 ?book }
-    }
-''')
+# Define your namespace
+pbs = Namespace('http://example.com/pbs#')
+mcc = Namespace('http://example.com/mcc#')
 
-# Print the URIs of the awards without mcc:R37 attribute
-for row in awards_without_r37:
-    award = row['award']
-    print(award)
+# Get all the awards of type MCC-E12 in the awards file
+awards_to_remove = set()
+for subj, _, _ in awards_graph.triples((None, None, mcc['MCC-E12'])):
+    awards_to_remove.add(subj)
+
+# Iterate over the triples in your hierarchy file
+for subj, pred, obj in list(hierarchy_graph.triples((None, SKOS.narrower, None))):
+    # If the object (award) is in the set of awards to remove, delete the triple
+    if obj in awards_to_remove:
+        hierarchy_graph.remove((subj, pred, obj))
+
+# Serialize the modified graph back to turtle
+hierarchy_graph.serialize(destination='modified_hierarchy.ttl', format='turtle')
