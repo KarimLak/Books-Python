c3b2fbd76b189a7b870b50cf7b4fdda354b41b0e
KarimLak <karim.lakhal@polymtl.ca>
2023-07-12 20:50:07 -0400

    Removing useless files

--- adding_years_constellation.py
+++ adding_years_constellation.py
@@ -1,14 +1,40 @@
-import re
+import requests
+from bs4 import BeautifulSoup
+from rdflib import Graph, Literal, BNode
+from rdflib.namespace import RDF, FOAF, XSD
+import rdflib.namespace
 
-input_file_path = "output_constellation.ttl"
-output_file_path = "output_constellation.ttl"
+# define the namespace
+pbs = rdflib.namespace.Namespace("http://www.example.org/pbs/#")
+ns1 = rdflib.namespace.Namespace("http://schema.org/")
 
-with open(input_file_path, 'r', encoding="utf-8") as file:
-    data = file.read()
+# load the graph
+g = Graph()
+g.parse("output_constellation.ttl", format="turtle")
 
-# find all occurrences of 'pbs:keyword "<keyword>" ;' and replace with 'pbs:keyword "<keyword>",'
-data = re.sub(r'(pbs:keyword ".*?") ;', r'\1,', data)
+# iterate over each book in the graph
+for book in g.subjects(RDF.type, ns1.Book):
+    # get the constellation link for the book
+    constellationLink = g.value(book, pbs.constellationLink)
 
-# write the corrected data to the output file
-with open(output_file_path, 'w',encoding="utf-8") as file:
-    file.write(data)
+    # get the page and parse it with BeautifulSoup
+    response = requests.get(constellationLink)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # extract the age ranges from the page
+    ageRanges = []
+    for td in soup.find_all('td'):
+        divs = td.find_all('div')  
+        for div in divs:
+            inner_divs = div.find_all('div', style=lambda value: value and 'background-color:#FFE699' in value)
+            if inner_divs:
+                ageRanges.append(div.get_text(strip=True))
+
+    # append the age ranges as attributes to the book
+    for ageRange in ageRanges:
+        g.add((book, ns1.ageRange, Literal(ageRange)))
+    
+    g.serialize(destination='output_constellation_updated.ttl', format='turtle')
+
+
+# save the graph
