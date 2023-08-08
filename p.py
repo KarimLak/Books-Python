fa1c940c4adfe9ed59a878a1d64c46afe9465ebf
KarimLak <karim.lakhal@polymtl.ca>
2023-08-07 22:32:22 -0400

    Publishers done

--- p.py
+++ p.py
@@ -1,28 +1,24 @@
-def read_file(file_name):
+import re
+
+def read_publishers_from_ttl(file_name):
     with open(file_name, 'r', encoding="utf-8") as f:
-        return [line.strip() for line in f.readlines()]
+        content = f.read()
 
-def extract_publishers_from_ttl(file_name):
-    # This is a simple extraction based on a presumption that publishers appear in a specific pattern in the TTL file.
-    # If the TTL structure is more complex, you might need a more sophisticated parsing mechanism.
-    publishers = set()
-    with open(file_name, 'r', encoding="utf-8") as f:
-        for line in f:
-            if 'publisher' in line:  # assuming a line containing 'publisher' holds the publisher info
-                # This extracts a string between double quotes. This might need to be adjusted based on the actual format.
-                publisher = line.split('"')[1] if '"' in line else None
-                if publisher:
-                    publishers.add(publisher.strip())
-    return publishers
+    # Extract publisher details
+    publishers = re.findall(r'ns1:(\w+) a ns1:Publisher ;\n\s+ns1:name "(.*?)"', content)
+    return {name: ttl_name for ttl_name, name in publishers}
 
-# Load publishers from both files
-known_publishers = set(read_file('publishers.txt'))
-ttl_publishers = extract_publishers_from_ttl('output_bnf.ttl')
+def replace_publishers_in_output_bnf(publishers):
+    with open('output_bnf.ttl', 'r', encoding="utf-8") as f:
+        content = f.read()
 
-# Find publishers in the known list that are not in the ttl and remove them
-to_keep = [publisher for publisher in known_publishers if publisher in ttl_publishers]
+    # Replace publishers
+    for publisher_name, ttl_name in publishers.items():
+        content = content.replace(f'ns1:publisher "{publisher_name}"', f'ns1:publisher ns1:{ttl_name}')
 
-# Write the filtered publishers back to publishers.txt
-with open('publishers.txt', 'w', encoding="utf-8") as f:
-    for publisher in to_keep:
-        f.write(publisher + '\n')
+    # Write back the updated content
+    with open('output_bnf.ttl', 'w', encoding="utf-8") as f:
+        f.write(content)
+
+publishers = read_publishers_from_ttl('publishers.ttl')
+replace_publishers_in_output_bnf(publishers)
