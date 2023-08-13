28e53102126cf3be307431ec4db472ee72822d16
Andre Diler <andre.diler@polymtl.ca>
2023-08-13 18:40:42 -0400

    hybridV2 works but needs Lurelu data

--- andre/db_alignments_hybrid.py
+++ andre/db_alignments_hybrid.py
@@ -11,8 +11,9 @@ import time
 
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-n_jobs = 12
-similarity_ratio = 0.95
+N_JOBS = 12
+SIMILARITY_RATIO = 1.0
+
 
 def setup_logger(name, log_file, level=logging.INFO):
     """To setup as many loggers as you want"""
@@ -26,32 +27,34 @@ def setup_logger(name, log_file, level=logging.INFO):
 
     return logger
 
-key_name = "name_author_date_publisher"
+
+key_name = "name_author_date"
 
 time_logger = setup_logger('execution_time_logger',
-                           f'{key_name}_approx_{similarity_ratio}_hybrid_execution_time_logfile.log')
-stats_logger = setup_logger('stats_logger', f'{key_name}_approx_{similarity_ratio}_hybrid_stats_logfile.log')
+                           f'{key_name}_approx_{SIMILARITY_RATIO}_hybrid_execution_time_logfile.log')
+stats_logger = setup_logger('stats_logger', f'{key_name}_approx_{SIMILARITY_RATIO}_hybrid_stats_logfile.log')
 
 # define the namespace
 pbs = rdflib.namespace.Namespace("http://www.example.org/pbs/#")
 ns1 = rdflib.namespace.Namespace("http://schema.org/")
 
 
-
 def is_key_close_enough_to_another_key(book_key, keys_to_check):
-    max_ratio  = 0
+    max_ratio = 0
     best_key = ""
-    for book in keys_to_check:
-        s = SequenceMatcher(None, book_key, book)
+    for key_to_check in keys_to_check:
+        s = SequenceMatcher(None, book_key, key_to_check)
         ratio = s.ratio()
-        if ratio >= similarity_ratio and ratio > max_ratio:
-            best_key = book
+        if ratio >= SIMILARITY_RATIO and ratio > max_ratio:
+            best_key = key_to_check
             max_ratio = ratio
     return best_key, max_ratio
+
 
 class BookAlignment:
     def __init__(self, isbn_constellation=None, isbn_bnf=None, age_range_constellation=None,
                  age_range_bnf=None, url_constellation=None, url_bnf=None):
+        self.key_used_to_align = None
         self.isbn_constellation = isbn_constellation
         self.isbn_bnf = isbn_bnf
         self.age_range_constellation = age_range_constellation  # list not used to conserve source in db
@@ -78,7 +81,7 @@ class InterDBStats:
         self.collision_number_exact_key: int = 0
         self.collision_number_approximate_key: int = 0
         self.key_type = key_type
-        self.all_book_alignments= {}
+        self.all_book_alignments = {}
 
     def increment_alignment_number(self):
         self.alignments_number += 1
@@ -87,7 +90,7 @@ class InterDBStats:
         self.collision_number_isbn += 1
 
     def increment_collision_number_exact_key(self):
-        self.collision_number_exact_key_key += 1
+        self.collision_number_exact_key += 1
 
     def increment_collision_number_approximate_key(self):
         self.collision_number_approximate_key += 1
@@ -98,19 +101,20 @@ class InterDBStats:
     def increment_constellation_book_number(self):
         self.constellation_book_number += 1
 
-    def align_hybrid(self,book_alignment, book_key):
-        aligned_with_isbn = self.align_by_key_isbn(book_alignment, book_alignment.isbn_bnf)
+    def align_hybrid(self, book_alignment, book_key):
+        aligned_with_isbn = self.align_by_key_isbn(book_alignment, book_key) # pass the key in case of non alignment
         if not aligned_with_isbn:
             aligned_with_book_key = self.align_by_key(book_alignment, book_key)
-            if not aligned_with_book_key:
-                self.align_by_approximate_key(book_alignment, book_key)
-
+            # if not aligned_with_book_key:
+            #     self.align_by_approximate_key(book_alignment, book_key)
 
     def align_by_approximate_key(self, book_alignment, book_key):
         keys_to_check = list(self.all_book_alignments.keys())
         start_key_finding = time.time()
-        batch_size = int(len(keys_to_check)/ n_jobs) + 1
-        similar_keys_list = parallel(delayed(is_key_close_enough_to_another_key)(book_key, keys_to_check[i:i + batch_size]) for i in range(0, len(keys_to_check), batch_size))
+        batch_size = int(len(keys_to_check) / N_JOBS) + 1
+        similar_keys_list = parallel(
+            delayed(is_key_close_enough_to_another_key)(book_key, keys_to_check[i:i + batch_size]) for i in
+            range(0, len(keys_to_check), batch_size))
         end_key_finding = time.time()
         if self.bnf_book_number % 10 == 0:
             time_logger.info(f"time elapsed key finding {end_key_finding - start_key_finding}")
@@ -126,15 +130,13 @@ class InterDBStats:
                 self.all_book_alignments[best_key].align(isbn_bnf=book_alignment.isbn_bnf,
                                                          age_range_bnf=book_alignment.age_range_bnf,
                                                          url_bnf=book_alignment.url_bnf,
-                                                         similarity_ratio = max_ratio,
-                                                         key_used_to_align = book_key)
+                                                         similarity_ratio=max_ratio,
+                                                         key_used_to_align=book_key)
                 self.increment_alignment_number()  # bnf data already present because of key doublon  inside bnf; independant of alignment (may be present without alignment_
                 print("align approximate")
 
             else:
                 self.increment_collision_number_approximate_key()  # increase if bnf data already present: doublon inside bnf
-
-
         else:
             self.all_book_alignments[book_key] = book_alignment  # bnf data gets into the dict without alignment
 
@@ -145,41 +147,44 @@ class InterDBStats:
                 return candidate_key
         return ""
 
-    def align_by_key_isbn(self, book_alignment, book_key_to_match):  # True if alignment in this call or already done (collision)
+    def align_by_key_isbn(self, book_alignment, book_key):  # True if alignment in this call or already done (collision)
         matched_key = self.isbn_alignment(book_alignment.isbn_bnf)
         if matched_key:
             if not self.all_book_alignments[matched_key].url_bnf:  # not a collision: bnf data not present
                 self.all_book_alignments[matched_key].align(isbn_bnf=book_alignment.isbn_bnf,
-                                                         age_range_bnf=book_alignment.age_range_bnf,
-                                                         url_bnf=book_alignment.url_bnf,
-                                                            similarity_ratio = -1,
+                                                            age_range_bnf=book_alignment.age_range_bnf,
+                                                            url_bnf=book_alignment.url_bnf,
+                                                            similarity_ratio=-1,
                                                             key_used_to_align="isbn")
                 self.increment_alignment_number()  # bnf data already present because of key doublon  inside bnf; independant of alignment (may be present without alignment_
                 print("align isbn")
                 return True
             else:
                 self.increment_collision_number_isbn()  # increase if bnf data already present: doublon inside bnf
-                return True # si collision -> deja aligné
+                return True  # si collision -> deja aligné
         else:
-            self.all_book_alignments[book_key_to_match] = book_alignment  # bnf data gets into the dict without alignment
+            self.all_book_alignments[
+                book_key] = book_alignment  # bnf data gets into the dict with its own key without alignment
             return False
 
-    def align_by_key(self, book_alignment, book_key_to_match):  # True if alignment in this call or already done (collision)
+    def align_by_key(self, book_alignment,
+                     book_key_to_match):  # True if alignment in this call or already done (collision)
         if book_key_to_match in self.all_book_alignments:
             if not self.all_book_alignments[book_key_to_match].url_bnf:  # not a collision: bnf data not present
                 self.all_book_alignments[book_key_to_match].align(isbn_bnf=book_alignment.isbn_bnf,
                                                                   age_range_bnf=book_alignment.age_range_bnf,
                                                                   url_bnf=book_alignment.url_bnf,
-                                                                  similarity_ratio = -1,
+                                                                  similarity_ratio=-1,
                                                                   key_used_to_align="exact")
                 self.increment_alignment_number()  # bnf data already present because of key doublon  inside bnf; independant of alignment (may be present without alignment_
                 print("align exact")
                 return True
             else:
                 self.increment_collision_number_exact_key()
-                return True # si collision -> deja aligné
+                return True  # si collision -> deja aligné
         else:
-            self.all_book_alignments[book_key_to_match] = book_alignment  # bnf data gets into the dict without alignment
+            self.all_book_alignments[
+                book_key_to_match] = book_alignment  # bnf data gets into the dict without alignment
             return False
 
     def is_isbn_in_alignments_bnf(self, target_isbn) -> bool:
@@ -190,12 +195,12 @@ class InterDBStats:
 
     def is_isbn_in_alignments(self, target_isbn, source) -> bool:
         if source == "bnf":
-            for book in self.all_book_alignments:
-                if target_isbn == self.all_book_alignments[book].isbn_bnf:
+            for book_alignment in self.all_book_alignments:
+                if target_isbn == self.all_book_alignments[book_alignment].isbn_bnf:
                     return True
-        elif source =="constellation":
-            for book in self.all_book_alignments:
-                if target_isbn == self.all_book_alignments[book].isbn_constellation:
+        elif source == "constellation":
+            for book_alignment in self.all_book_alignments:
+                if target_isbn == self.all_book_alignments[book_alignment].isbn_constellation:
                     return True
         else:
             stats_logger.error(f"wrong source in is_isbn_in_alignments, {source} doesn't exist")
@@ -204,16 +209,16 @@ class InterDBStats:
     def compute_alignment_confusion_matrix(self):
 
         stats_logger.info("-----")
-        isbn_equality = 0 # TP
-        isbn_inequality = 0 # FP
+        isbn_equality = 0  # TP
+        isbn_inequality = 0  # FP
         missing_isbn_bnf_in_negatives = 0
         missing_isbn_constellation_in_negatives = 0
         missing_isbn_constellation_in_positives = 0
         missing_isbn_bnf_in_positives = 0
-        total_non_alignment = 0 # N
-        total_alignment = 0 # P
-        non_alignment_isbn_present = 0 # FN
-        non_alignment_isbn_absent = 0 # TN
+        total_non_alignment = 0  # N
+        total_alignment = 0  # P
+        non_alignment_isbn_present = 0  # FN
+        non_alignment_isbn_absent = 0  # TN
         lines_without_url = 0
         for book_key in self.all_book_alignments:
             isbn_constellation = self.all_book_alignments[book_key].isbn_constellation
@@ -233,14 +238,14 @@ class InterDBStats:
                     missing_isbn_constellation_in_positives += 1
                 if not isbn_bnf:
                     missing_isbn_bnf_in_positives += 1
-            elif url_bnf and not url_constellation: # non-alignment: only 1 bnf present
+            elif url_bnf and not url_constellation:  # non-alignment: only 1 bnf present
                 total_non_alignment += 1
                 if isbn_bnf:
                     # look if isbn present in a constellation book
                     if self.is_isbn_in_alignments_constellation(isbn_bnf):
                         non_alignment_isbn_present += 1
                     else:
-                        non_alignment_isbn_absent +=1
+                        non_alignment_isbn_absent += 1
                 else:
                     missing_isbn_bnf_in_negatives += 1
             elif not url_bnf and url_constellation:
@@ -248,15 +253,15 @@ class InterDBStats:
                 if isbn_constellation:
                     # look if isbn present in a bnf book
                     if self.is_isbn_in_alignments_bnf(isbn_constellation):
-                        non_alignment_isbn_present +=1
+                        non_alignment_isbn_present += 1
                     else:
-                        non_alignment_isbn_absent +=1
+                        non_alignment_isbn_absent += 1
                 else:
                     # missing isbn
-                    missing_isbn_constellation_in_negatives +=1
-            else: #no url at all
+                    missing_isbn_constellation_in_negatives += 1
+            else:  # no url at all
                 lines_without_url += 1
-                stats_logger("empty line", book)
+                stats_logger.info("empty line", book)
 
         stats_logger.info(f"total alignment P {total_alignment}")
         stats_logger.info(f"total non-alignment N {total_non_alignment}")
@@ -302,7 +307,8 @@ class InterDBStats:
         # print(f"average similarity between ages of same {self.key_type}", average_similarity / len(aligned_books))
 
     def output_csv(self):
-        with open(f"hybrid_alignment_{self.key_type}_ratio_{similarity_ratio}.csv", "w", encoding='utf-8', newline="") as csvfile:
+        with open(f"hybrid_alignment_{self.key_type}_ratio_{SIMILARITY_RATIO}.csv", "w", encoding='utf-8',
+                  newline="") as csvfile:
             writer = csv.writer(csvfile, delimiter='{')
             writer.writerow(["key",
                              "similarity ratio",
@@ -353,20 +359,18 @@ for book in g.subjects(RDF.type, ns1.Book):
                                                  age_range_constellation=book_data.age_range_int)
 
     # name_author_key = utils.create_key(book_name, book_author)
-    isbn_key = book_data.isbn
+    # isbn_key = book_data.isbn
     # name_author_publisher_key = utils.create_key(book_name, book_author, publisher)
-    name_author_publisher_date_key = utils.create_key(book_name=book_data.book_name,
+    # name_author_publisher_date_key = utils.create_key(book_name=book_data.book_name,
+    #                                                   book_author=book_data.book_author,
+    #                                                   publisher=book_data.publisher,
+    #                                                   publication_date=book_data.publication_date)
+    name_author_date_key = utils.create_key(book_name=book_data.book_name,
                                             book_author=book_data.book_author,
-                                            publisher=book_data.publisher,
                                             publication_date=book_data.publication_date)
-    # name_author_date_key = utils.create_key(book_name=book_data.book_name,
-    #                                         book_author=book_data.book_author,
-    #                                         publication_date=book_data.publication_date)
 
-
-    stats.all_book_alignments[name_author_publisher_date_key] = \
+    stats.all_book_alignments[name_author_date_key] = \
         copy.deepcopy(book_alignment_constellation)
-
 
     stats.increment_constellation_book_number()
 
@@ -382,7 +386,7 @@ g.parse("../output_bnf.ttl", format="turtle")
 
 # BNF loop
 
-with Parallel(n_jobs=n_jobs) as parallel:
+with Parallel(n_jobs=N_JOBS) as parallel:
     for book in g.subjects(RDF.type, ns1.Book):  # O(M)
         book_data: utils.RdfBookData = \
             utils.remove_special_chars(
@@ -394,17 +398,16 @@ with Parallel(n_jobs=n_jobs) as parallel:
                                            isbn_bnf=book_data.isbn,
                                            age_range_bnf=book_data.age_range_int)
 
-        name_author_publisher_date_key = utils.create_key(book_name=book_data.book_name,
-                                                          book_author=book_data.book_author,
-                                                          publisher=book_data.publisher,
-                                                          publication_date=book_data.publication_date)
-        # name_author_date_key = utils.create_key(book_name=book_data.book_name,
-        #                                         book_author=book_data.book_author,
-        #                                         publication_date=book_data.publication_date)
-
+        # name_author_publisher_date_key = utils.create_key(book_name=book_data.book_name,
+        #                                                   book_author=book_data.book_author,
+        #                                                   publisher=book_data.publisher,
+        #                                                   publication_date=book_data.publication_date)
+        name_author_date_key = utils.create_key(book_name=book_data.book_name,
+                                                book_author=book_data.book_author,
+                                                publication_date=book_data.publication_date)
 
         start = time.time()
-        stats.align_hybrid(copy.deepcopy(book_alignment_bnf), name_author_publisher_date_key)
+        stats.align_hybrid(copy.deepcopy(book_alignment_bnf), name_author_date_key)
         end = time.time()
         if stats.bnf_book_number % 10 == 0:
             time_logger.info(f"book no {stats.bnf_book_number}")
@@ -416,7 +419,6 @@ with Parallel(n_jobs=n_jobs) as parallel:
         # stats_isbn.increment_bnf_book_number()
         # stats_name_author_publisher.increment_bnf_book_number()
         stats.increment_bnf_book_number()
-
 
 # stats_name_author.output_csv()
 # stats_isbn.output_csv()
